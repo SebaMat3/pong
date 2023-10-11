09b0af424f17fdc7e759e7da7fb3286f925f957f
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-10-10 21:03:17 -0300

    player movement control updating

--- Player.gd
+++ Player.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
-
-
+const SPEED = 400
 
 func _physics_process(delta):
 	#initial direction == Vector2(0,0)
 	var direction = Vector2.ZERO
-	var speed = 400
-	#keyboard up|down pressing events listening
-	if Input.is_action_pressed("ui_up"):
-		direction.y -=1	
-	if Input.is_action_pressed("ui_down"):
-		direction.y +=1                                         
-		
-	move_and_slide(speed * direction)
+	
+	# Handle up movement.     
+	if Input.is_action_pressed("ui_up"):         
+		direction.y -= direction * SPEED     
+	# Handle down movement.     
+	if Input.is_action_pressed("ui_down"):         
+		direction.y += direction * SPEED     
+	
+		#shorter way to state it
+	#var direction = Input.get_axis("ui_up", "ui_down")     
+	#velocity.y = direction * SPEED      
+	
+	move_and_slide()
 
